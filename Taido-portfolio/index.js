c2f3970bc0bbd67b5d4f9476cb5e582cac8d886d
Thato-Raphahlelo <68586559+Thato-Raphahlelo@users.noreply.github.com>
2021-05-28 16:47:49 +0200

    Add files via upload

---
+++ Taido-portfolio/index.js
@@ -0,0 +1,15 @@
+
+function hideSide(){
+    var element = document.getElementById('SC');
+
+        element.style.transition = "opacity 0.3s ease-in-out top 0.3s ease-in-out";
+        element.style.opacity = "0";
+        element.style.top = "-100%";
+}
+
+function showSide(){
+    var element = document.getElementById('SC');
+    element.style.transition = "opacity 0.3s ease-in-out top 0.3s ease-in-out";
+    element.style.opacity = "1";
+    element.style.top = "0%";
+}
